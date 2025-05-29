05323a1562e1daa4b00416f09a162cd0c0bea2aa
z3770 <augnyachev@ya.ru>
2025-05-29 12:06:31 +0300

    init

---
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      style = "night",
+    },
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "tokyonight-night",
+    },
+  },
+}
---
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -0,0 +1,23 @@
+return {
+  "stevearc/oil.nvim",
+  lazy = false,
+  opts = {},
+  keys = {
+    {
+      "<leader>fe",
+      function()
+        require("oil").open(LazyVim.root())
+      end,
+      desc = "Explorer Oil (root dir)",
+    },
+    {
+      "<leader>fE",
+      function()
+        require("oil").open()
+      end,
+      desc = "Explorer Oil (cwd)",
+    },
+    { "<leader>e", "<leader>fe", desc = "Explorer Oil (root dir)", remap = true },
+    { "<leader>E", "<leader>fE", desc = "Explorer Oil (cwd)", remap = true },
+  },
+}
